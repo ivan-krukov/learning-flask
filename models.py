a62580488f7bb11f808106801afac0fa0acf1949
Ivan Kryukov <ivan.o.kryukov@gmail.com>
2017-01-15 00:57:00 -0700

    form view

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
 
-    uid = db.COlumn(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(100))
     lastname = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True)
